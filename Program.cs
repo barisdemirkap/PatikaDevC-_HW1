43761e2089f09b26cef9e26f159733ecfa76edfb
barisdemirkap <barisdemirkap@gmail.com>
2022-01-01 20:25:12 +0300

    questin 3 try

--- Program.cs
+++ Program.cs
@@ -28,31 +28,58 @@ namespace Homework1
         //
         //}
 
-    class Question2
+    //class Question2
+    //{
+    //    static void Main(string[] args)
+    //    {   Console.WriteLine("Pozitif birinci sayi girin") ;
+    //        int n = int.Parse(Console.ReadLine()) ;
+    //        Console.WriteLine("Pozitif ikinci sayi girin") ;
+    //        int m = int.Parse(Console.ReadLine()) ;
+    //        List<int> numbers = new List<int>() ;
+    //
+    //        for (int i=0; i<n ; i++)
+    //        {
+    //           numbers.Add(int.Parse(Console.ReadLine())) ;
+    //        }
+    //         for (int i=0; i<n ; i++)
+    //           {
+    //              if(numbers[i]%m==0)
+    //                   {
+    //                       Console.WriteLine("bolunen sayi: " + numbers[i]);
+    //                   }
+    //            
+    //    
+    //
+    //
+    //    }
+    //
+    //    Console.ReadLine() ;
+    //    }
+    //}
+    class Question3
     {
         static void Main(string[] args)
-        {   Console.WriteLine("Pozitif birinci sayi girin") ;
-            int n = int.Parse(Console.ReadLine()) ;
-            Console.WriteLine("Pozitif ikinci sayi girin") ;
-            int m = int.Parse(Console.ReadLine()) ;
-            List<int> numbers = new List<int>() ;
-
-            for (int i=0; i<n ; i++)
-            {
-               numbers.Add(int.Parse(Console.ReadLine())) ;
-            }
-             for (int i=0; i<n ; i++)
-               {
-                  if(numbers[i]%m==0)
-                       {
-                           Console.WriteLine("bolunen sayi: " + numbers[i]);
-                       }
+        {       
+                Console.WriteLine("Pozitif bir sayi girin") ;
+                int n = int.Parse(Console.ReadLine()) ;
+                List<string> users = new List<string>() ;
+                Console.WriteLine("kullanici isimlerini girin") ;
+                for(int i = 0; i<n ; i++)
+                {   
+                    users.Add(Convert.ToString(Console.ReadLine())) ; 
+                }
                 
-        
-    
+                users.Reverse();
+                Console.WriteLine(String.Join(',', users)) ;
+               // for(int i = 0; i<n ; i++)
+                //{   
+                //   Console.WriteLine(users[i]) ;
+                //   
+                //}
+                Console.ReadLine() ;
 
-        }
-        }
+
+    }
     }
 }
 
