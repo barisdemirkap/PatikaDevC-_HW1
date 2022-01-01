6437127ebf7128f38f6330811e60c99533b2a887
barisdemirkap <barisdemirkap@gmail.com>
2022-01-01 19:54:37 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Homework1
+{
+  //  class Question1
+    //{
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Pozitif bir sayi girin") ;
+        //   int n = int.Parse(Console.ReadLine()) ;
+        //
+        //    List <int> numbers = new List<int>() ;
+        //    for (int i=0; i<n ; i++)
+        //    {
+        //       numbers.Add(int.Parse(Console.ReadLine())) ;
+        //    }
+//
+        //    for (int i=0; i<n ; i++)
+        //    {
+        //       if(numbers[i]%2==0)
+        //            {
+        //                Console.WriteLine("cift sayi: " + numbers[i]);
+        //            }
+        //    }
+//
+        //}
+        
+    
+    
+    //}
+
+    class Question2
+    {
+        public void Main(string[] args)
+        {   Console.WriteLine("Pozitif iki sayi girin") ;
+            int n = int.Parse(Console.ReadLine()) ;
+            int m = int.Parse(Console.ReadLine()) ;
+            List<int> numbers = new List<int>() ;
+
+            for (int i=0; i<n ; i++)
+            {
+               numbers.Add(int.Parse(Console.ReadLine())) ;
+            }
+             for (int i=0; i<n ; i++)
+               {
+                  if(numbers[i]%m==0)
+                       {
+                           Console.WriteLine("bolunen sayi: " + numbers[i]);
+                       }
+                
+        
+    
+
+        }
+        }
+    }
+}
+
+    
